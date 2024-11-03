d9c4a2658c8c6a201f300b62de6df6055174d1a6
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-11-03 13:06:07 +0500

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let rollBtn = document.getElementById("rollBtn")
 let hold = document.getElementById("hold")
-let newGame = document.getElementById("newgame")
+let newGame = document.querySelector("#newgame")
 let img = document.getElementById("img")
 let player1Area = document.getElementById("player1Area")
 let player2Area = document.getElementById("player2Area")
@@ -30,12 +30,10 @@ const togglePlayer = () => {
 
 rollBtn.addEventListener('click', () => {
     img.style.display = "block";
-    let diceValue = Math.floor(Math.random() * 5 + 1);
-    img.src = `./assets/${diceValue}.webp`;
+    let diceValue = Math.floor(Math.random() * 6 + 1);
+    img.src = `./assets/${diceValue}.png`;
 
     if (diceValue === 1) {
-        rollBtn.disabled = true;
-        setTimeout(() => rollBtn.disabled = false, 1000);
         score1 = score2 = 0;
         userOneScore.innerHTML = score1;
         userTwoScore.innerHTML = score2;
@@ -61,10 +59,12 @@ hold.addEventListener('click', () => {
     userTwoScore.innerHTML = score2;
 
     if (totalScore1 >= 100 || totalScore2 >= 100) {
-        rollBtn.disabled = hold.disabled = true;
-        img.style.display = "none";
-        winner.innerHTML = totalScore1 >= 100 && totalScore2 >= 100 ? "Match is tied" :
-            totalScore1 >= 100 ? "Player 1 is winner" : "Player 2 is winner";
+        document.querySelector('.mainArea').style.display = 'none'
+        if (totalScore1 >= 100) {
+            winner.innerHTML = 'Player one is winner'
+        } else if (totalScore2 >= 100) {
+            winner.innerHTML = 'Player 2 is Winner'
+        }
         winner.style.display = "block";
     } else {
         togglePlayer();
@@ -72,7 +72,8 @@ hold.addEventListener('click', () => {
 });
 
 newGame.addEventListener('click', () => {
-    switchPlayer = true;
+    document.querySelector('.mainArea').style.display = 'flex'
+    switchPlayer = false;
     togglePlayer();
     totalScore1 = totalScore2 = score1 = score2 = 0;
     userOneRes.innerHTML = totalScore1;
@@ -80,6 +81,4 @@ newGame.addEventListener('click', () => {
     userOneScore.innerHTML = score1;
     userTwoScore.innerHTML = score2;
     winner.style.display = "none";
-    rollBtn.disabled = hold.disabled = false;
-    img.style.display = "none";
 });
